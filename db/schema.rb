ae90df5585b0c887657a0c24d2483b387b5bf083
Duc Nghiem Xuan <nghiem.xuan.duc@framgia.com>
2015-02-06 13:28:18 +0700

    Add sign up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205091205) do
+ActiveRecord::Schema.define(version: 20150206061629) do
 
   create_table "answers", force: true do |t|
     t.text     "answer_1"
@@ -58,8 +58,9 @@ ActiveRecord::Schema.define(version: 20150205091205) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   create_table "words", force: true do |t|
