5d6cae937298aa35a5665e6888c654192dd82ebc
nguyenthai <nguyen.tan.thai@framgia.com>
2015-03-05 15:54:22 +0700

    Fix filter selected

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,2 +1,13 @@
 module WordsHelper
+  def category_selected category
+    unless category.nil?
+      [@category.name, @category.id]
+    end
+  end
+
+  def category_list
+    [["All", ""]] + Category.all.collect do |category|
+      [category.name, category.id]
+    end
+  end
 end
