793dbe94c8b721291331561a152a15a3f90c48c4
nguyenthai <nguyen.tan.thai@framgia.com>
2015-03-06 10:26:53 +0700

    fix lession update

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -2,7 +2,7 @@ module ActivitiesHelper
   def get_user user_id
     user = User.find user_id
     user_name =  current_user?(user) ? "You" : user.name
-    link_to gravatar_for(user, size: 25, name: user_name), user
+    link_to gravatar_for(user, size: 25, name: user_name) + user_name, user
   end
 
   def get_category_by_lession lession_id
