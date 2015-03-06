aa171a9cac36467b04478c27fd14a1d1a0c97907
nguyenthai <nguyen.tan.thai@framgia.com>
2015-03-06 17:36:39 +0700

    Change form_for to form_tag filter

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,19 +1,17 @@
 class WordsController < ApplicationController
   before_action :logged_in_user
 
-  helper WordsHelper
-
   def index
-    category_id = params[:word] ? params[:word][:category_id] : nil
-    @learned = params[:word] ? params[:word][:learned].to_i : nil
-    @words = if category_id.blank?
+    @category_id = params[:category_id]
+    @learned = params[:learned].to_i
+    @words = if @category_id.blank?
       if @learned == 1 || @learned == 0
        current_user.send "#{'not_' if @learned == 0}learned_words"
       else
         Word.all
       end
     else
-      @category = Category.find category_id
+      @category = Category.find @category_id
       if @learned == 1 || @learned == 0
         @category.send "words#{'_not' if @learned == 0}_learned_by", current_user
       else
--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,13 +1,2 @@
 module WordsHelper
-  def category_selected category
-    unless category.nil?
-      [@category.name, @category.id]
-    end
-  end
-
-  def category_list
-    [["All", ""]] + Category.all.collect do |category|
-      [category.name, category.id]
-    end
-  end
 end
